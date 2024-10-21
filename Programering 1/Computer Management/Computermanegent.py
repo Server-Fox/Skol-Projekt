8bcfb373bd68d494e5f7151eb9baac6a2e3055db
Server-Fox <ericv03@live.se>
2024-10-21 14:21:32 +0200

    Progress so far

---
+++ Programering 1/Computer Management/Computermanegent.py
@@ -0,0 +1,8 @@
+
+computerListFull = [f"Dell", "HP", "Lenovo", "Macbook", "Cromebook", "Alienware"]
+computerListDeployed = ()
+computerListService = ()
+computerListFull.sort()
+
+for computer in computerListFull:
+    print(computer)
